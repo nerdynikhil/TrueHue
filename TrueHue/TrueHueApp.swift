775bfc867b8457351d2a099b263527b27a583190
Nikhil Barik <nklmarch17@gmail.com>
2025-08-05 23:55:16 +0530

    Initial Commit

---
+++ TrueHue/TrueHueApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrueHueApp.swift
+//  TrueHue
+//
+//  Created by Nikhil Barik on 05/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TrueHueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
